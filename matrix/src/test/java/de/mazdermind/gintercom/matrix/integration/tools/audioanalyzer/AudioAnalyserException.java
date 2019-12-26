0a2ffbbda4f7a44b2b3aad790e1e1a620a670210
MaZderMind <git@mazdermind.de>
2019-12-26 19:29:11 +0100

    add new anylysis tooling

---
+++ matrix/src/test/java/de/mazdermind/gintercom/matrix/integration/tools/audioanalyzer/AudioAnalyserException.java
@@ -0,0 +1,11 @@
+package de.mazdermind.gintercom.matrix.integration.tools.audioanalyzer;
+
+class AudioAnalyserException extends RuntimeException {
+	public AudioAnalyserException(String message) {
+		super(message);
+	}
+
+	public AudioAnalyserException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
