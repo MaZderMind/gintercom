e1517cdb3df85145be4a7e37b31043a954be9d62
MaZderMind <git@mazdermind.de>
2020-03-06 18:36:19 +0100

    add a last-resort exit for a hanging wait

--- src/main/java/de/mazdermind/gintercom/shared/pipeline/support/VoidFuture.java
+++ src/main/java/de/mazdermind/gintercom/shared/pipeline/support/VoidFuture.java
@@ -2,6 +2,8 @@ package de.mazdermind.gintercom.shared.pipeline.support;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class VoidFuture {
 
@@ -17,8 +19,8 @@ public class VoidFuture {
 
 	public void await() {
 		try {
-			future.get();
-		} catch (InterruptedException | ExecutionException e) {
+			future.get(1, TimeUnit.MINUTES);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
 			throw new RuntimeException(e);
 		}
 	}
