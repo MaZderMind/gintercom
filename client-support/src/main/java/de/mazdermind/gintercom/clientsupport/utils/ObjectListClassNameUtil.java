6649e690274863576302b146f431e8926acd4f0a
Peter Körner <pkoerner@seibert-media.net>
2020-05-09 00:32:22 +0200

    remove unused code

--- client-support/src/main/java/de/mazdermind/gintercom/clientsupport/utils/ObjectListClassNameUtil.java
+++ client-support/src/main/java/de/mazdermind/gintercom/clientsupport/utils/ObjectListClassNameUtil.java
@@ -1,16 +1,9 @@
 package de.mazdermind.gintercom.clientsupport.utils;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class ObjectListClassNameUtil {
-	public static List<String> classNames(Collection<?> objects) {
-		return objects.stream()
-			.map(o -> o.getClass().getSimpleName())
-			.collect(Collectors.toList());
-	}
-
 	public static String classNamesList(Collection<?> objects) {
 		return objects.stream()
 			.map(o -> o.getClass().getSimpleName())
