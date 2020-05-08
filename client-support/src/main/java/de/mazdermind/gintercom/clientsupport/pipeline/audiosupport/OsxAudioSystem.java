1c1b43943c1add0a8ed732323c12f956c9b7e25a
Peter Körner <pkoerner@seibert-media.net>
2020-05-08 23:20:10 +0200

    log a warning wharning about the audio-rate on OSX

--- client-support/src/main/java/de/mazdermind/gintercom/clientsupport/pipeline/audiosupport/OsxAudioSystem.java
+++ client-support/src/main/java/de/mazdermind/gintercom/clientsupport/pipeline/audiosupport/OsxAudioSystem.java
@@ -4,7 +4,10 @@ import org.freedesktop.gstreamer.Element;
 import org.freedesktop.gstreamer.ElementFactory;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class OsxAudioSystem implements AudioSystem {
 	private static final String SINK_ELEMENT = "osxaudiosink";
 	private static final String SRC_ELEMENT = "osxaudiosrc";
@@ -17,6 +20,7 @@ public class OsxAudioSystem implements AudioSystem {
 
 	@Override
 	public Element buildSourceElement() {
+		log.info("On OSX the Audio-Device *has* to be set to 48kHz Manually!");
 		Element src = ElementFactory.make(SRC_ELEMENT, SRC_ELEMENT);
 		//src.set("blocksize", GstConstants.BYTES_PER_BUFFER);
 		return src;
