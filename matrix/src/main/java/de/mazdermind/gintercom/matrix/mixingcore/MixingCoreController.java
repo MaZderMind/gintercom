5ed87cc4c2ef427309ab7b51f044412e262977b6
MaZderMind <git@mazdermind.de>
2020-07-23 00:07:49 +0200

    apply MembershipChangeMessage to MixingCore

--- matrix/src/main/java/de/mazdermind/gintercom/matrix/mixingcore/MixingCoreController.java
+++ matrix/src/main/java/de/mazdermind/gintercom/matrix/mixingcore/MixingCoreController.java
@@ -5,11 +5,15 @@ import javax.annotation.PreDestroy;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import de.mazdermind.gintercom.clientapi.configuration.ButtonDirection;
+import de.mazdermind.gintercom.clientapi.configuration.ButtonTargetType;
+import de.mazdermind.gintercom.clientapi.controlserver.messages.client.to.matrix.MembershipChangeMessage;
 import de.mazdermind.gintercom.matrix.controlserver.ClientAssociation;
 import de.mazdermind.gintercom.matrix.events.ClientAssociatedEvent;
 import de.mazdermind.gintercom.matrix.events.ClientDeAssociatedEvent;
-import de.mazdermind.gintercom.mixingcore.MixingCore;
 import de.mazdermind.gintercom.mixingcore.Client;
+import de.mazdermind.gintercom.mixingcore.Group;
+import de.mazdermind.gintercom.mixingcore.MixingCore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,9 +40,36 @@ public class MixingCoreController {
 	public void handleClientDeAssociatedEvent(ClientDeAssociatedEvent clientDeAssociatedEvent) {
 		ClientAssociation association = clientDeAssociatedEvent.getAssociation();
 
-		log.debug("Removing Panel {}", association.getClientId());
+		log.debug("Removing Client {}", association.getClientId());
 		Client client = mixingCore.getClientById(association.getClientId());
 		mixingCore.removeClient(client);
+	}
+
+	@EventListener
+	public void handleMembershipChangeMessage(MembershipChangeMessage.ClientMessage clientMessage) {
+		MembershipChangeMessage membershipChangeMessage = clientMessage.getMessage();
+		log.info("Received MembershipChangeMessage Message from {}: {}", clientMessage.getClientId(), membershipChangeMessage);
+
+		Client client = mixingCore.getClientById(clientMessage.getClientId());
+
+		MembershipChangeMessage.Change change = membershipChangeMessage.getChange();
+		ButtonDirection direction = membershipChangeMessage.getDirection();
+
+		if (membershipChangeMessage.getTargetType() == ButtonTargetType.GROUP) {
+			Group group = mixingCore.getGroupById(membershipChangeMessage.getTarget());
+
+			if (change == MembershipChangeMessage.Change.JOIN && direction == ButtonDirection.RX) {
+				client.startReceivingFrom(group);
+			} else if (change == MembershipChangeMessage.Change.LEAVE && direction == ButtonDirection.RX) {
+				client.stopReceivingFrom(group);
+			} else if (change == MembershipChangeMessage.Change.JOIN && direction == ButtonDirection.TX) {
+				client.startTransmittingTo(group);
+			} else if (change == MembershipChangeMessage.Change.LEAVE && direction == ButtonDirection.TX) {
+				client.stopTransmittingTo(group);
+			}
+		} else if (membershipChangeMessage.getTargetType() == ButtonTargetType.PANEL) {
+			log.error("Panel-to-Panel-Messages are not yet implemented");
+		}
 	}
 
 	@PreDestroy
