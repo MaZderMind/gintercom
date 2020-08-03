1fa31fedef61e6460b07f7a9fd23d8e2208b8e98
MaZderMind <git@mazdermind.de>
2020-08-03 08:59:12 +0200

    fix ui lint & test issues

--- matrix/src/ui/src/app/pages/groups/group-actions/group-actions.component.spec.ts
+++ matrix/src/ui/src/app/pages/groups/group-actions/group-actions.component.spec.ts
@@ -2,6 +2,8 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {GroupActionsComponent} from './group-actions.component';
 import {RouterTestingModule} from '@angular/router/testing';
+import {GroupsService} from 'src/app/services/groups/groups.service';
+import {instance, mock} from 'ts-mockito';
 
 describe('GroupActionsComponent', () => {
   let component: GroupActionsComponent;
@@ -11,6 +13,9 @@ describe('GroupActionsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [GroupActionsComponent],
       imports: [RouterTestingModule],
+      providers: [
+        {provide: GroupsService, useFactory: () => instance(mock(GroupsService))},
+      ],
     })
       .compileComponents();
   }));
--- matrix/src/ui/src/app/pages/groups/group-actions/group-actions.component.ts
+++ matrix/src/ui/src/app/pages/groups/group-actions/group-actions.component.ts
@@ -2,7 +2,6 @@ import {Component, Input} from '@angular/core';
 import {GroupDto} from 'src/app/services/groups/group-dto';
 import {GroupsService} from 'src/app/services/groups/groups.service';
 import {MessageService} from 'src/app/messages/message.service';
-import {UsageDto} from 'src/app/services/usage-dto';
 
 @Component({
   selector: 'app-group-actions',
@@ -22,11 +21,11 @@ export class GroupActionsComponent {
   deleteGroup() {
     this.groupsService.getGroupUsage(this.group.id).then(usage => {
       if (usage.used) {
-        this.messageService.showWarning(`Group ${this.group.id} still in use by: ${usage.users.join(', ')}`)
+        this.messageService.showWarning(`Group ${this.group.id} still in use by: ${usage.users.join(', ')}`);
       } else {
         this.groupsService.deleteGroup(this.group.id)
-          .then(() => this.messageService.showInfo(`Group ${this.group.id} deleted successfully`))
+          .then(() => this.messageService.showInfo(`Group ${this.group.id} deleted successfully`));
       }
-    })
+    });
   }
 }
