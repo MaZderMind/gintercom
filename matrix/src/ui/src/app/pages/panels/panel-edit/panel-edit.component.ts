7355fae5e59d9473c7babb036b97e049b0022581
MaZderMind <git@mazdermind.de>
2020-08-04 23:30:57 +0200

    add basic form for panels

--- matrix/src/ui/src/app/pages/panels/panel-edit/panel-edit.component.ts
+++ matrix/src/ui/src/app/pages/panels/panel-edit/panel-edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-panel-edit',
@@ -7,15 +8,26 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./panel-edit.component.scss']
 })
 export class PanelEditComponent implements OnInit {
-  selectedId: string;
+  panelEditForm = new FormGroup({
+    id: new FormControl('', [Validators.required]),
+    display: new FormControl(''),
+  });
+
+  panelId: string;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.selectedId = params.get('id');
+      this.panelId = params.get('id');
     });
   }
 
+  isCreation(): boolean {
+    return !this.panelId;
+  }
+
+  onSubmit() {
+  }
 }
