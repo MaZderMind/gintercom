2a048a9bc4b33c96dd51635658db3ea3da655a1d
MaZderMind <git@mazdermind.de>
2020-08-13 23:02:29 +0200

    cache decorator and test

---
+++ matrix/src/ui/src/app/utils/cache-decorator.spec.ts
@@ -0,0 +1,122 @@
+import {Cache} from 'src/app/utils/cache-decorator';
+import {Observable, of} from 'rxjs';
+import {delay, share} from 'rxjs/operators';
+import {fakeAsync, flush, tick} from '@angular/core/testing';
+
+class ExampleService {
+  calls: string[] = [];
+
+  @Cache(20)
+  literalGet(id: string): string {
+    this.calls.push('call-' + id);
+    return 'result-of-' + id;
+  }
+
+  @Cache(20)
+  promiseGet(id: string): Promise<string> {
+    this.calls.push('call-' + id);
+    return of('result-of-' + id)
+      .pipe(delay(150))
+      .toPromise();
+  }
+
+  @Cache(20)
+  subscriptionGet(id: string): Observable<string> {
+    this.calls.push('call-' + id);
+    return of('result-of-' + id)
+      .pipe(share(), delay(150));
+  }
+}
+
+fdescribe('cache-decorator', () => {
+  let exampleService: ExampleService;
+
+  beforeEach(() => {
+    exampleService = new ExampleService();
+  });
+
+  it('does only execute once within the cache-timeout', fakeAsync(() => {
+    exampleService.literalGet('foo');
+    exampleService.literalGet('foo');
+    exampleService.literalGet('foo');
+
+    expect(exampleService.calls).toEqual(['call-foo']);
+
+    flush();
+  }));
+
+  it('executes once for each argument', fakeAsync(() => {
+    const a = exampleService.literalGet('foo');
+    const b = exampleService.literalGet('bar');
+    const c = exampleService.literalGet('foo');
+
+    expect(a).toEqual('result-of-foo');
+    expect(b).toEqual('result-of-bar');
+    expect(c).toEqual('result-of-foo');
+
+    expect(exampleService.calls).toEqual(['call-foo', 'call-bar']);
+
+    flush();
+  }));
+
+  it('re-executes after the cache-timeout', fakeAsync(() => {
+    exampleService.literalGet('foo');
+    exampleService.literalGet('bar');
+    expect(exampleService.calls).toEqual(['call-foo', 'call-bar']);
+
+    tick(15);
+    exampleService.literalGet('foo');
+    expect(exampleService.calls).toEqual(['call-foo', 'call-bar']);
+
+    tick(15);
+    exampleService.literalGet('foo');
+    expect(exampleService.calls).toEqual(['call-foo', 'call-bar', 'call-foo']);
+
+    flush();
+  }));
+  describe('with literal return-value', () => {
+    it('returns the literal result on each call', fakeAsync(() => {
+      const a = exampleService.literalGet('foo');
+      const b = exampleService.literalGet('foo');
+      const c = exampleService.literalGet('foo');
+
+      expect(a).toEqual('result-of-foo');
+      expect(b).toEqual('result-of-foo');
+      expect(c).toEqual('result-of-foo');
+
+      flush();
+    }));
+  });
+
+  describe('with promise return-value', () => {
+    it('returns the promise result on each call', fakeAsync(async () => {
+      const a = exampleService.promiseGet('foo');
+      const b = exampleService.promiseGet('foo');
+      const c = exampleService.promiseGet('foo');
+
+      tick(200);
+
+      expect(await a).toEqual('result-of-foo');
+      expect(await b).toEqual('result-of-foo');
+      expect(await c).toEqual('result-of-foo');
+
+      flush();
+    }));
+  });
+
+  describe('with subscription return-value', () => {
+    it('returns the promise result on each call', fakeAsync(async () => {
+      const a = exampleService.subscriptionGet('foo');
+      const b = exampleService.subscriptionGet('foo');
+      const c = exampleService.subscriptionGet('foo');
+
+      a.subscribe(ar => expect(ar).toEqual('result-of-foo'));
+      b.subscribe(br => expect(br).toEqual('result-of-foo'));
+      c.subscribe(cr => expect(cr).toEqual('result-of-foo'));
+
+      tick(200);
+
+      flush();
+    }));
+  });
+});
---
+++ matrix/src/ui/src/app/utils/cache-decorator.ts
@@ -0,0 +1,60 @@
+/**
+ * Debounce a method
+ *
+ * class SearchBarComponent {
+ *   constructor(private httpClient: HttpClient) {
+ *   }
+ *
+ *   @Cache(50)
+ *   getFoo(arg: bar) {
+ *       return this.httpClient.get("/api/search?q=" + searchText).toPromise();
+ *   }
+ * }
+ * </code>
+ */
+import * as _ from 'lodash';
+
+interface CacheEntry {
+  callArgs: IArguments;
+  result: any;
+}
+
+function log(...args: any[]) {
+  // nop
+}
+
+export function Cache(maxAgeMs) {
+  const cache: CacheEntry[] = [];
+
+  return (target: any, key: any, descriptor: any) => {
+    const oldFunc = descriptor.value;
+
+    descriptor.value = function() {
+      log('looking for cached entry for', arguments);
+      const cachedEntry = cache.find(cacheEntry =>
+        _.isEqual(cacheEntry.callArgs, arguments));
+
+      if (cachedEntry) {
+        log('found cached entry for', arguments);
+        return cachedEntry.result;
+      }
+
+      log('executing oldFunc for', arguments);
+      const executionResult = oldFunc.apply(this, arguments);
+      log('executionResult for', arguments, 'is', executionResult);
+
+      log('creating cache-entry for', arguments);
+      const newCacheEntry = {callArgs: arguments, result: executionResult};
+      cache.push(newCacheEntry);
+
+      log('registering cache-result removal for', arguments);
+      setTimeout(() => {
+        log('removing cache-entry for', arguments);
+        _.pull(cache, newCacheEntry);
+      }, maxAgeMs);
+
+      log('returning executionResult for', arguments);
+      return executionResult;
+    };
+  };
+}
