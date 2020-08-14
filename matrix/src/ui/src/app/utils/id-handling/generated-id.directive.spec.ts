7749ff6dd3106ffd1eff3899ab45c4f7f22a9779
MaZderMind <git@mazdermind.de>
2020-08-14 22:22:56 +0200

    ui unit test for GeneratedIdDirective

--- matrix/src/ui/src/app/utils/id-handling/generated-id.directive.spec.ts
+++ matrix/src/ui/src/app/utils/id-handling/generated-id.directive.spec.ts
@@ -1,8 +1,36 @@
 import {GeneratedIdDirective} from './generated-id.directive';
 
 describe('GeneratedIdDirective', () => {
+  let elementSpy;
+  let elementRef;
+  let directive: GeneratedIdDirective;
+
+  beforeEach(() => {
+    elementSpy = jasmine.createSpyObj('element', ['setAttribute']);
+    elementRef = {nativeElement: elementSpy};
+
+    directive = new GeneratedIdDirective(elementRef);
+  });
+
   it('should create an instance', () => {
-    const directive = new GeneratedIdDirective(null);
     expect(directive).toBeTruthy();
+  });
+
+  it('should generate an id', () => {
+    directive.ngOnInit();
+    expect(elementSpy.setAttribute).toHaveBeenCalledWith('id', jasmine.stringMatching(/^n[0-9]+$/));
+  });
+
+  it('should generate different ids for each Element', () => {
+    directive.ngOnInit();
+    const arg1 = elementSpy.setAttribute.calls.mostRecent().args[1];
+
+    const directive2 = new GeneratedIdDirective(elementRef);
+    directive2.ngOnInit();
+    const arg2 = elementSpy.setAttribute.calls.mostRecent().args[1];
+
+    expect(arg1).toMatch(/^n[0-9]+$/);
+    expect(arg2).toMatch(/^n[0-9]+$/);
+    expect(arg1).not.toEqual(arg2);
   });
 });
