755b3219c210cd1d4ecc5779695b345ed36b89e4
MaZderMind <git@mazdermind.de>
2020-08-14 22:07:46 +0200

    ui-test for autofocus directive

--- matrix/src/ui/src/app/utils/autofocus.directive.spec.ts
+++ matrix/src/ui/src/app/utils/autofocus.directive.spec.ts
@@ -1,8 +1,31 @@
 import {AutofocusDirective} from 'src/app/utils/autofocus.directive';
 
 describe('FocusDirective', () => {
+  let elementSpy;
+  let elementRef;
+  let directive: AutofocusDirective;
+
+  beforeEach(() => {
+    elementSpy = jasmine.createSpyObj('element', ['focus']);
+    elementRef = {nativeElement: elementSpy};
+
+    directive = new AutofocusDirective(elementRef);
+  });
   it('should create an instance', () => {
-    const directive = new AutofocusDirective(null);
     expect(directive).toBeTruthy();
+  });
+
+  it('should call .focus() when autoFocus is true', () => {
+    directive.autofocus = true;
+    directive.ngAfterViewInit();
+
+    expect(elementSpy.focus).toHaveBeenCalled();
+  });
+
+  it('should not call .focus() when autoFocus is false', () => {
+    directive.autofocus = false;
+    directive.ngAfterViewInit();
+
+    expect(elementSpy.focus).not.toHaveBeenCalled();
   });
 });
