b76eb460e9c441bc8adbced84579fa811b08ce94
MaZderMind <git@mazdermind.de>
2020-08-14 20:39:10 +0200

    documentstion on cache decorator

--- matrix/src/ui/src/app/utils/cache-decorator.ts
+++ matrix/src/ui/src/app/utils/cache-decorator.ts
@@ -21,6 +21,25 @@ interface CacheEntry {
 
 const DEFAULT_CACHE_DURATION = 150;
 
+/**
+ * Cache-Decorator for Service-Methods
+ *
+ * By Applying this Decorator to a get*-Method of a Service-Class, especially such services that hit a HTTP-Backend with one or more
+ * Requests per Call, the Result of the Get-Operation still stay valid for the given amount of time. This results in the HTTP_Call only
+ * being executed once, even if the get-Method is invoked multiple times within the given maxAge.
+ *
+ * This happens often, when Components loading auxiliary Data (ie a Select-Box that loads the Set of available Options) are used
+ * multiple times within the same Widget. The Cache-Decorator reduces the number of Calls to 1 while still alowing the Components to
+ * request their auxiliary Data themself.
+ *
+ * The Cache-Decorator takes the given Arguments into account and stores the Result only once for each unique combination of arguments.
+ * It is therefore safe to use the Decorator on a get+-Method that takes one or more Arguments.
+ *
+ * The Cache-Decorator works with native Result-Types and Object-Type as well as with Promises and Subscriptions, as long as the
+ * Subscriptions are shared by calling `.pipe(share())` on them.
+ *
+ * @param maxAgeMs Amount of Time the Method-Result is valid (given in milliseconds)
+ */
 export function Cache(maxAgeMs = DEFAULT_CACHE_DURATION) {
   const cache: CacheEntry[] = [];
 
